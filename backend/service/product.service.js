8644e255de7003ebc6c19c1ebc5e909daffba508
VladZyma <vladzima13@gmail.com>
2023-04-08 18:01:04 +0300

    added product filters by query

--- backend/service/product.service.js
+++ backend/service/product.service.js
@@ -5,9 +5,50 @@ const productService = {
     return Product.create(productInfo);
   },
   findAllProducts: async (query) => {
-    const {limit = 6, page = 1} = query;
+    const {limit = 6, page = 1, title, priceMin, priceMax, color, size, category, tags} = query;
 
     let findObj = {};
+
+    if (title) {
+      const titleArr = title.split(' ');
+
+      const normalizedTitleArr = titleArr.map(title => title.charAt(0).toUpperCase() + title.slice(1).toLowerCase());
+      const normalizedTitle = normalizedTitleArr.join(' ');
+
+      findObj = {...findObj, title: {$regex: normalizedTitle}};
+    }
+
+    if (priceMin && priceMax) {
+      findObj = {$and: [
+          {...findObj},
+          {$or: [{'price.current': {$lte: priceMax, $gte: priceMin}}, {'price.sale': {$lte: priceMax, $gte: priceMin}}] },
+        ]};
+    }
+
+    if (color) {
+      const colorArr = color.split(',');
+
+      findObj = {$and: [
+          {...findObj},
+          {color: {$in: colorArr}}
+        ]};
+    }
+
+    if (size) {
+      const normalizedSize = size.toUpperCase();
+
+      findObj = {...findObj,  info: {$elemMatch: {size: normalizedSize}}}
+    }
+
+    if (category) {
+      findObj = {...findObj, categories: {$in: [category]}}
+    }
+
+    if (tags) {
+      const tagsArr = tags.split(',');
+
+      findObj = {...findObj, tags: {$in: tagsArr}}
+    }
 
     const [products, count] = await Promise.all([
         Product.find(findObj).limit(limit).skip((+page - 1) * limit).lean(),
