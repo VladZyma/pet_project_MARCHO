f43984a6afd13689ccb6b26d16a0c2ce5f0aa505
VladZyma <vladzima13@gmail.com>
2023-04-22 09:34:00 +0300

    added userId to local storage

--- frontend/src/service/oauth.service.js
+++ frontend/src/service/oauth.service.js
@@ -4,16 +4,18 @@ import {urls} from "../config";
 const _accessTokenKey = 'accessToken';
 const _refreshTokenKey = 'refreshToken';
 const _userName = 'userName';
+const _userId = 'userId';
 
 const oauthService = {
   register: (user) => axiosService.post(urls.register, user),
   login: (user) => axiosService.post(urls.auth.login, user),
   refresh: (refresh) => axiosService.post(urls.auth.refresh, {refresh}),
 
-  setAccessTokens: ({accessToken, refreshToken, name}) => {
+  setAccessTokens: ({accessToken, refreshToken, name, userId}) => {
     localStorage.setItem(_accessTokenKey, accessToken);
     localStorage.setItem(_refreshTokenKey, refreshToken);
     localStorage.setItem(_userName, name);
+    localStorage.setItem(_userId, userId);
   },
   getAccessToken: () => {
     return localStorage.getItem(_accessTokenKey);
